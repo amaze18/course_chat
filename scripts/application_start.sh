0fc077f606a948212c79baab0f8b17b61736fcc5
Anupam <92312729+amaze18@users.noreply.github.com>
2024-03-09 13:57:09 +0530

    Update application_start.sh

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 # Start Streamlit application
-streamlit run /opt/codedeploy-agent/deployment-root/streamlit_app.py
+nohup streamlit run /opt/codedeploy-agent/deployment-root/streamlit_app.py &
+
