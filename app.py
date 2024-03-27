c4d037b17fe60338946a1711533a00fc20cf98e5
Kush Juvekar <119339573+Koosh0610@users.noreply.github.com>
2024-03-27 19:42:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+
 import warnings
 warnings.filterwarnings("ignore")
 import time
@@ -5,6 +6,7 @@ from rouge import Rouge
 import streamlit as st
 from streamlit_lottie import st_lottie
 import os
+import io
 import boto3
 from botocore.exceptions import NoCredentialsError
 import nest_asyncio
@@ -16,12 +18,14 @@ import os
 import openai
 openai.api_key=os.environ['SECRET_TOKEN']
 
+
 from llama_index.core import SimpleDirectoryReader
 from llama_index.extractors.entity import EntityExtractor
 from llama_index.core.node_parser import SentenceSplitter
 from llama_index.core.ingestion import IngestionPipeline
 from llama_index.core import ServiceContext
 from llama_index.core import VectorStoreIndex
+from llama_index.legacy.core.llms.types import ChatMessage, MessageRole
 from llama_index.llms.openai import OpenAI
 from llama_index.core import StorageContext, load_index_from_storage
 from llama_index.core.memory import ChatMemoryBuffer
@@ -37,21 +41,34 @@ from llama_index.legacy.postprocessor import LongContextReorder
 from llama_index.legacy.schema import MetadataMode
 from llama_index.legacy.schema import QueryBundle
 from llama_index.legacy import (StorageContext,load_index_from_storage)
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+import pandas as pd
+
+st.set_page_config(page_title="VidyaRANG: Learning Made Easy",page_icon="/home/ubuntu/vidyarang.jpg",layout="centered")
 DEFAULT_CONTEXT_PROMPT_TEMPLATE = """
-  The following is a friendly conversation between a user and an AI assistant.
+  You're VidyaRANG. An AI assistant developed by members of AIGurukul to help students learn their course material via convertsations.
+  The following is a friendly conversation between a user and an AI assistant for answering questions related to query.
   The assistant is talkative and provides lots of specific details from its context only.
   Here are the relevant documents for the context:
 
   {context_str}
 
-  Instruction: Based on the above context, provide a detailed answer with logical formation of paragraphs for the user question below.
-  Answer "don't know" if information is not present in context. Also, decline to answer questions that are not related to context."
+  Instruction: Based on the above context, provide a crisp answer IN THE USER'S LANGUAGE with logical formation of paragraphs for the user question below.
+  Strict Instruction: Answer "I don't know." if information is not present in context. Also, decline to answer questions that are not related to context."
   """
-
+condense_prompt = (
+  "Given the following conversation between a user and an AI assistant and a follow up question from user,"
+  "rephrase the follow up question to be a standalone question.\n"
+  "Chat History:\n"
+  "{chat_history}"
+  "\nFollow Up Input: {question}"
+  "\nStandalone question:")
 
 s3_bucket_name="coursechat"
 access_key = os.environ.get('ACCESS_ID')
 secret_key = os.environ.get('ACCESS_KEY')
+auth_token = os.environ.get('auth_token')
 # Set your AWS credentials and region (replace with your own values)
 AWS_ACCESS_KEY = access_key
 AWS_SECRET_KEY =secret_key
@@ -69,26 +86,69 @@ import pandas as pd
 from io import StringIO
 st.title("VidyaRANG: Learning Made Easy")
 #st.cache_data.clear()
-auth_token = os.environ.get("auth_token")
-allowed_emails_csv_path = "allowed_emails.csv"
-allowed_emails_df = pd.read_csv('allowed_emails.csv')
-allowed_emails_set = set(allowed_emails_df['email'].str.lower())
 
-#__login__obj = __login__(auth_token = "courier_auth_token", 
-      ##             width = 200, height = 250, 
-        #            logout_button_name = 'Logout', hide_menu_bool = False, 
-         #           hide_footer_bool = False, 
-          #          lottie_url = 'https://assets2.lottiefiles.com/packages/lf20_jcikwtux.json')
+REGION = 'us-east-1'
+BUCKET_NAME = 'access-coursechat' 
+s3c = boto3.client(
+        's3', 
+        region_name = REGION,aws_access_key_id=AWS_ACCESS_KEY ,aws_secret_access_key=AWS_SECRET_KEY
+    )
+def update_users():
+    # Step 1: Access Google Sheet
+    # Define the scope
+    scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+
+    # Add credentials to the account
+    creds = ServiceAccountCredentials.from_json_keyfile_name('trans-shuttle-418411-c40767daa58c.json', scope)
+
+    # Authorize the client
+    client = gspread.authorize(creds)
+
+    # Open the Google Sheet by its URL
+    url = 'https://docs.google.com/spreadsheets/d/1iDDl36tUIhTRWN4P2aCA7FUuBKXzuKHndmK4mtn3DOA/edit?resourcekey#gid=1112975212'
+    worksheet = client.open_by_url(url).sheet1
+
+    # Step 2: Read Google Sheet into pandas DataFrame
+    # Get all values from the worksheet
+    data = worksheet.get_all_values()
+
+    # Convert to DataFrame
+    headers = data.pop(0)
+    df = pd.DataFrame(data, columns=headers)
+
+    allowed_emails_df = pd.read_csv('allowed_emails.csv')
+
+    # Extract the existing emails from the DataFrame
+    existing_emails = set(allowed_emails_df['email'])
+
+    # Extract emails from the 'E-mail ID' column of the DataFrame
+    emails_to_check = set(df['E-mail ID'])
+
+    # Find emails that are not in allowed_emails.csv
+    new_emails = emails_to_check - existing_emails
+
+    # Append new emails to allowed_emails.csv
+    with open('allowed_emails.csv', 'a') as file:
+        for email in new_emails:
+            file.write(f"{email}\n")
+update_users()
+#obj = s3c.get_object(Bucket= BUCKET_NAME , Key = "allowed_emails.csv")
+#df = pd.read_csv(io.BytesIO(obj['Body'].read()), encoding='utf8')
+df = pd.read_csv("allowed_emails.csv", encoding='utf8')
+#allowed_emails_csv_path = "allowed_emails.csv"
+#allowed_emails_df = pd.read_csv('allowed_emails.csv')
+allowed_emails_set = set(df['email'].str.lower())
+
 
 __login__obj = __login__(auth_token = auth_token, 
-                    company_name = "Evaluatingcodes",
+                    company_name = "VidyaRANG",
                     width = 200, height = 250, 
                     logout_button_name = 'Logout', hide_menu_bool = False, 
                     hide_footer_bool = False, 
                     lottie_url = "https://assets2.lottiefiles.com/packages/lf20_jcikwtux.json",
                     allowed_emails_set = allowed_emails_set)
 
-LOGGED_IN = __login__obj.build_login_ui()
+LOGGED_IN , username = __login__obj.build_login_ui()
 def create_s3_subfolder(course_name):
     s3 = boto3.client(
         "s3",
@@ -146,7 +206,6 @@ def indexgenerator(indexPath, documentsPath):
     if not os.path.exists(indexPath):
         print("Not existing")
         # load the documents and create the index
-        
 
         entity_extractor = EntityExtractor(prediction_threshold=0.2,label_entities=False, device="cpu") # set device to "cuda" if gpu exists
         node_parser = SentenceSplitter(chunk_overlap=102,chunk_size=1024)
@@ -156,7 +215,7 @@ def indexgenerator(indexPath, documentsPath):
 
         pipeline = IngestionPipeline(transformations=transformations)
         nodes = pipeline.run(documents=documents)
-        service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo", temperature=0),embed_model=embed_model)
+        service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo-0125", temperature=0),embed_model=embed_model)
         index = VectorStoreIndex(nodes, service_context=service_context)
 
         # store it for later
@@ -222,14 +281,21 @@ def get_indexed_course_list():
        return x
     except:
         return []
-def course_chat(option):
+    
+def course_chat(option,username=username):
     embed_model = OpenAIEmbedding(model="text-embedding-ada-002")
     indexPath=f"/home/ubuntu/Indices/{option}"  
-    st.write(indexPath)
     storage_context = StorageContext.from_defaults(persist_dir=indexPath)
     index = load_index_from_storage(storage_context,service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo", temperature=0),embed_model=embed_model))
     vector_retriever = VectorIndexRetriever(index=index,similarity_top_k=2)
-    bm25_retriever = BM25Retriever.from_defaults(index=index, similarity_top_k=2)
+    bm25_flag = True
+    try:
+        bm25_retriever = BM25Retriever.from_defaults(index=index,similarity_top_k=2)
+    except:
+        source_nodes = index.docstore.docs.values()
+        nodes = list(source_nodes)
+        bm25_flag = False
+        #bm25_retriever = BM25Retriever.from_defaults(nodes=nodes,similarity_top_k=2)
     postprocessor = LongContextReorder()
     rouge = Rouge()
     class HybridRetriever(BaseRetriever):
@@ -245,14 +311,23 @@ def course_chat(option):
             query = str(query)
             all_nodes = postprocessor.postprocess_nodes(nodes=all_nodes,query_bundle=QueryBundle(query_str=query.lower()))
             return all_nodes[0:2]
-    hybrid_retriever=HybridRetriever(vector_retriever,bm25_retriever)
-    llm = OpenAI(model="gpt-3.5-turbo")
+
+    if bm25_flag:
+        hybrid_retriever=HybridRetriever(vector_retriever,bm25_retriever)
+    else:
+        hybrid_retriever=vector_retriever
+    if username == "GB123" or username == "Koosh0610" or username == "anupam.aiml@gmail.com" or username == "Helloworld" :
+        llm = OpenAI(model="gpt-4-1106-preview")
+    else:
+        llm = OpenAI(model="gpt-3.5-turbo-0125")
     service_context = ServiceContext.from_defaults(llm=llm)
     query_engine=RetrieverQueryEngine.from_args(retriever=hybrid_retriever,service_context=service_context,verbose=True)
     if "messages" not in st.session_state.keys(): # Initialize the chat messages history
         st.session_state.messages = [{"role": "assistant", "content": "Ask me a question from the course you have selected!!"}]
+    if "message_history" not in st.session_state.keys():
+        st.session_state.message_history=[ChatMessage(role=MessageRole.ASSISTANT,content="Ask me a questioin form the course you have selected"),]
     if "chat_engine" not in st.session_state.keys(): # Initialize the chat engine
-        st.session_state.chat_engine = CondensePlusContextChatEngine.from_defaults(query_engine,context_prompt=DEFAULT_CONTEXT_PROMPT_TEMPLATE)
+        st.session_state.chat_engine = CondensePlusContextChatEngine.from_defaults(query_engine,context_prompt=DEFAULT_CONTEXT_PROMPT_TEMPLATE,condense_prompt=condense_prompt,chat_history=st.session_state.message_history)
     if prompt := st.chat_input("Your question"): # Prompt for user input and save to chat history
         st.session_state.messages.append({"role": "user", "content": str(prompt)})
     for message in st.session_state.messages: # Display the prior chat messages
@@ -280,6 +355,7 @@ def course_chat(option):
                     df.to_csv(csv_buffer)
                     s3_resource= boto3.resource('s3',aws_access_key_id=os.environ["ACCESS_ID"],aws_secret_access_key=os.environ["ACCESS_KEY"])
                     s3_resource.Object(bucket, option+'_course_logs.csv').put(Body=csv_buffer.getvalue())
+                    st.session_state.message_history.append(ChatMessage(role=MessageRole.ASSISTANT,content=str(response.response)),)
                 except:
                     df = pd.DataFrame(columns=['Question','Answer','Unigram_Recall','Unigram_Precision','Bigram_Recall','Bigram_Precision','Time'])
                     new_row = {'Question': str(prompt), 'Answer': response.response,'Unigram_Recall' : scores[0]["rouge-1"]["r"],'Unigram_Precision' : scores[0]["rouge-1"]["p"],'Bigram_Recall' : scores[0]["rouge-2"]["r"],'Bigram_Precision' : scores[0]["rouge-2"]["r"],"Time" : end-start}
@@ -290,6 +366,7 @@ def course_chat(option):
                     df.to_csv(csv_buffer)
                     s3_resource= boto3.resource('s3',aws_access_key_id=os.environ["ACCESS_ID"],aws_secret_access_key=os.environ["ACCESS_KEY"])
                     s3_resource.Object(bucket, '{option}_course_logs.csv').put(Body=csv_buffer.getvalue())
+                    st.session_state.message_history.append(ChatMessage(role=MessageRole.ASSISTANT,content=str(response.response)),)
                 message = {"role": "assistant", "content": response.response}
                 st.session_state.messages.append(message) # Add response to message history
 
@@ -302,10 +379,9 @@ def upload_files(course_name, download_path = '/home/ubuntu'):
     #course_name = st.text_input("Enter course name:")
 
     # File uploader
-    uploaded_file_list = st.file_uploader("Choose your files", type=["pdf", "txt", "csv","doc","docx","xls","xlsx",'ppt',"pptx","html"], accept_multiple_files=True)
+    uploaded_file_list = st.file_uploader("Choose your files (Uploader of the file confirms that the content being uploaded is original and does not violate any copyrights, granting permission for its distribution.)", type=["pdf", "txt", "csv","doc","docx","xls","xlsx"], accept_multiple_files=True)
 
     if st.button("Upload your files") and uploaded_file_list:
-        # Upload file to S3
         for uploaded_file in uploaded_file_list:
             upload_to_s3(course_name, uploaded_file)
         download_from_s3(course_name)
@@ -341,10 +417,9 @@ def get_files_in_directory(bucket_name, directory):
 
 def chat_reset(option):
     st.session_state.messages = [{"role": "assistant", "content": "Ask me a question from the course you have selected!!"}]
-    #Reset the chat_engine in session state
     if 'chat_engine' in st.session_state:
-      del st.session_state.chat_engine
-         
+        del st.session_state.chat_engine
+
 ## MAIN FUNCTION ##
 def main():
     
@@ -364,7 +439,27 @@ def main():
            option= st.selectbox("Select course",tuple(get_indexed_course_list()))
            chat_reset(option)
            course_chat(option)
-            
+      #st.footer("The user is solely responsible for uploading any content that may be subject to copyright.")
+      st.markdown(
+        """
+        <style>
+        .footer {
+            position: fixed;
+            bottom: 0;
+            left: 0;
+            width: 100%;
+            background-color: #000000;
+            padding: 10px 0;
+            text-align: center;
+            z-index: 9999;
+        }
+        </style>
+        <div class="footer">
+            The user is solely responsible for uploading any content that may be subject to copyright.
+        </div>
+        """,
+        unsafe_allow_html=True
+     )
 # Push the directory to GitHub
 if __name__ == "__main__":
     main()
