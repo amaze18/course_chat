75f6b1b4396dfb556e1c7ce1ef9c9b275d6e9de9
Chitran-0249f <59654086+Chitran-0249f@users.noreply.github.com>
2024-05-27 17:15:59 +0530

    Update validate_service.sh

--- scripts/validate_service.sh
+++ scripts/validate_service.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Check if Streamlit application is running
-response=$(curl -s -o /dev/null -w "%{http_code}" http://100.25.194.156:8080/)
+response=$(curl -s -o /dev/null -w "%{http_code}" http://3.83.225.186:8080/)
 if [[ "$response" == "200" ]]; then
     echo "Streamlit application is running."
 else
